a3b43cfe2ecb00ad36afce5de54a7495012e569d
Bibest <bibhestee@gmail.com>
2023-07-11 15:16:09 +0100

    Create: return an asyncio class task

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" Asyncio Task """
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int):
+    """
+        task_wait_random - create an asyncio task and return it
+
+        Arguments:
+            max_delay (int) - delayed seconds duration.
+        Return:
+            asyncio.Task
+    """
+    return asyncio.Task(wait_random(max_delay))
