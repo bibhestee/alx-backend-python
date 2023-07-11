5a982847dda392b75e0a533706fb0581d2714823
Bibest <bibhestee@gmail.com>
2023-07-11 19:40:27 +0100

    annotate return variable

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """ Async comprehension for coroutine """
 async_generator = __import__('0-async_generator').async_generator
+from typing import List
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """
         async_comprehension - The coroutine will collect 10 random numbers
             using an async comprehensing over async_generator,
