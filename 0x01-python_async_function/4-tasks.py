b2b19d803c4b18b70102c8620a9c0294b439dc1f
Bibest <bibhestee@gmail.com>
2023-07-11 15:41:57 +0100

    multiple task wait coroutines

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" Multiple task wait coroutines at the same time """
+from typing import List
+import asyncio
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """ task_wait_n - spawn wait random n times
+        Arguments:
+            n (int) - number of times to spawn wait random
+            max_delay (int) - delayed seconds duration.
+        Return:
+            list of delayed values
+    """
+    done, pend = await asyncio.wait([task_wait_random(max_delay) for i in range(n)])
+    results = [task.result() for task in done]
+    return sorted(results)
