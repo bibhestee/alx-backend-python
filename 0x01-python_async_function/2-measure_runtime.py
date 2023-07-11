770a09f87be7c2ba7bf0e11c0a06fcb8f3c06c55
Bibest <bibhestee@gmail.com>
2023-07-11 11:39:19 +0100

    replace time.time with time.perf_counter

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -14,8 +14,8 @@ def measure_time(n: int, max_delay: int) -> float:
         Return:
             total time / n
     """
-    start = time.time()
+    start = time.perf_counter()
     asyncio.run(wait_n(n, max_delay))
-    stop = time.time()
+    stop = time.perf_counter()
     total_time = stop - start
     return total_time/n
