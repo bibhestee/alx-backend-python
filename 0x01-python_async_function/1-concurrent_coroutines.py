05a83dead86eb0bfc6e46560bf4cd04552082ba4
Bibest <bibhestee@gmail.com>
2023-07-11 07:32:13 +0100

    anotation for params added

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" Multiple coroutines at the same time with async """
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int):
+    """ Wait n - spawn wait random n times
+        Arguments:
+            n (int) - number of times to spawn wait random
+            max_delay (int) - delayed seconds duration.
+        Return:
+            list of delayed values
+    """
+    delays = []
+    for i in range(n+1):
+        delays.append(await wait_random(max_delay))
+    return delays
