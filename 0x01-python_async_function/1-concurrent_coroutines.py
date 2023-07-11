3ad7102db4d4a2d269d495afef4098f49c164d65
Bibest <bibhestee@gmail.com>
2023-07-11 11:05:57 +0100

    Add list from typing for return variable

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """ Multiple coroutines at the same time with async """
-
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """ Wait n - spawn wait random n times
         Arguments:
             n (int) - number of times to spawn wait random
--- 0x01-python_async_function/README.md
+++ 0x01-python_async_function/README.md
@@ -3,3 +3,4 @@
 ## PROJECTS
 
 - [0-basic_async_syntax.py](/0x01-python_async_function/0-basic_async_syntax.py)
+- [1-concurrent_coroutines.py](/0x01-python_async_function/1-concurrent_coroutines.py)
