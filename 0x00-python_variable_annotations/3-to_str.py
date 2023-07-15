55af44ad4a75716bedec9da9f3bfb9cab8e0f092
Bibest <bibhestee@gmail.com>
2023-07-15 22:59:55 +0100

    to string function with typed annotation

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" A function that returns a str representation of a float """
+
+
+def to_str(n: float) -> str:
+    """ to_str - takes a float n as argument and returns the
+                string representation of the float.
+        Arguments:
+            n (float) - argument
+        Return:
+            string representation of the float
+    """
+    return str(n)
