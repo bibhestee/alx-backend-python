9badd97eb208c2718e95e9831e23e70d0f588823
Bibest <bibhestee@gmail.com>
2023-07-16 10:43:48 +0100

    typed annotation to float arg and return float

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" A function that takes a float and returns the floor of the float """
+import math
+
+
+def floor(n: float) -> float:
+    """ floor - takes a float n as argument and returns the floor of the float
+        Arguments:
+            n (float) - argument
+        Return:
+            the floor of the float
+    """
+    return math.floor(n)
