70c558097e0d05ded695916693a5c071c4ee662f
Bibest <bibhestee@gmail.com>
2023-07-16 11:36:38 +0100

    allow list to take multiple types using union

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """ A function that takes a list of floats and ints and returns a float """
-from typing import List
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[int, float, None]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
     """ sum_mixed_list - takes a list mxd_lst of integers and
                         floats and returns their sum as a float.
         Arguments:
