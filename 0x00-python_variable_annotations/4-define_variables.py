c318bcd1826c6f555b2f42952de47bbbeb204742
Bibest <bibhestee@gmail.com>
2023-07-16 10:44:56 +0100

    typed annotation to variables

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" Define and annotate the variables with appropriate values """
+
+# a, an integer with a value of 1
+a: int = 1
+# pi, a float with a value of 3.14
+pi: float = 3.14
+# i_understand_annotations, a boolean with a value of True
+i_understand_annotations: bool = True
+# school, a string with a value of “Holberton”
+school: str = "Holberton"
