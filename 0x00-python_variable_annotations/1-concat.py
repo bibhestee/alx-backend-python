86c1aac0ccb0e2ff66a97dd1c83f538e24fc0234
Bibest <bibhestee@gmail.com>
2023-07-16 10:42:49 +0100

    typed annotation to str args and return str

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" A function thtat takes a two strings and return a concat str """
+
+
+def concat(str1: str, str2: str) -> str:
+    """ concat- takes a string str1 and a string str2 as arguments
+                and returns a concatenated string
+        Arguments:
+            str1 (str) - first string
+            str2 (str) - second string
+        Return:
+            a concatenated string
+    """
+    return str1 + str2
