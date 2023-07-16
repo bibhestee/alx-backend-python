81f2f83da0174c4db0fbce7f1077098b2cc4e9ec
Bibest <bibhestee@gmail.com>
2023-07-16 11:22:04 +0100

    typed annotation to mixed args and returns a float

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" A function that takes a list of floats and ints and returns a float """
+from typing import List
+
+
+def sum_mixed_list(mxd_lst: List[int, float, None]) -> float:
+    """ sum_mixed_list - takes a list mxd_lst of integers and
+                        floats and returns their sum as a float.
+        Arguments:
+            mxd_lst (list) - list of floats and ints
+        Return:
+           sum of the floats and ints
+    """
+    result = 0
+    for num in mxd_lst:
+        result += num
+    return result
