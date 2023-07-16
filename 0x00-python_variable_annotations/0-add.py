708c9541c950b488516be9cf08bb6e6860f11567
Bibest <bibhestee@gmail.com>
2023-07-16 10:36:56 +0100

    typed annotation to int arg and return statement

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" A function that takes a float and returns a float """
+
+
+def add(a: float, b: float) -> float:
+    """ add - takes a float a and a float b as arguments and returns their sum as a float.
+        Arguments:
+            a (float) - first number
+            b (float) - second number
+        Return:
+            sum of the arguments as a float
+    """
+    return a + b
